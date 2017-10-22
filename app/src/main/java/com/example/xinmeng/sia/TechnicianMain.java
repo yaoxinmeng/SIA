858590781bc21354669d96a8feded5dae7b50cab
yaoxinmeng <yaoxinmeng@gmail.com>
2017-10-22 16:30:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xinmeng/sia/TechnicianMain.java
@@ -0,0 +1,27 @@
+package com.example.xinmeng.sia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TechnicianMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_technician_main);
+
+
+    }
+
+    public void tasks(View view)
+    {
+
+    }
+
+    public void equipment(View view)
+    {
+
+    }
+}
