7eb5889557ddbfb0c9240becd5e099ba10f477c2
yaoxinmeng <yaoxinmeng@gmail.com>
2017-10-22 18:32:56 +0800

    Initial commit

--- app/src/main/java/com/example/xinmeng/sia/TechnicianTasksMain.java
+++ app/src/main/java/com/example/xinmeng/sia/TechnicianTasksMain.java
@@ -1,7 +1,9 @@
 package com.example.xinmeng.sia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TechnicianTasksMain extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class TechnicianTasksMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_technician_tasks_main);
+    }
+
+    public void taskDetail(View view)
+    {
+        Intent intent = new Intent(TechnicianTasksMain.this, TechnicianTaskDetail.class);
+        startActivity(intent);
     }
 }
