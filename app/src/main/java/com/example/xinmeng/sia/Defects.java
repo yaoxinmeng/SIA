f2ac9c1b22f248a72cf72871c3eca925c4c17356
yaoxinmeng <yaoxinmeng@gmail.com>
2017-10-22 22:31:06 +0800

    new class

---
+++ app/src/main/java/com/example/xinmeng/sia/Defects.java
@@ -0,0 +1,12 @@
+package com.example.xinmeng.sia;
+
+/**
+ * Created by Xin Meng on 22/10/2017.
+ */
+
+public class Defects {
+    public String name;
+    public String description;
+    public int seatNumber;
+    public String equipment;
+}
--- app/src/main/java/com/example/xinmeng/sia/Plane.java
+++ app/src/main/java/com/example/xinmeng/sia/Plane.java
@@ -1,8 +1,14 @@
 package com.example.xinmeng.sia;
 
+
 /**
  * Created by Xin Meng on 22/10/2017.
  */
 
 public class Plane {
+    public String location;
+    public float landingTime;
+    public float takeoffTIme;
+
+    public Defects[] defects;
 }
