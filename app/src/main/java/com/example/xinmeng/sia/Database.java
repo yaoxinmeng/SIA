5ee7913efe50f5a00a7e9e38534fbf624420554f
yaoxinmeng <yaoxinmeng@gmail.com>
2017-10-22 23:45:33 +0800

    new class

---
+++ app/src/main/java/com/example/xinmeng/sia/Database.java
@@ -0,0 +1,13 @@
+package com.example.xinmeng.sia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Xin Meng on 22/10/2017.
+ */
+
+public class Database {
+    public static List defects = new ArrayList<Defects>();
+    public static List planes = new ArrayList<Plane>();
+}
--- app/src/main/java/com/example/xinmeng/sia/Plane.java
+++ app/src/main/java/com/example/xinmeng/sia/Plane.java
@@ -1,6 +1,9 @@
 package com.example.xinmeng.sia;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Xin Meng on 22/10/2017.
  */
@@ -10,7 +13,21 @@ public class Plane {
     public float landingTime;
     public float takeoffTIme;
     public int flightNumber;
-    public Defects[] defects;
+    public List defects = new ArrayList<Defects>();
 
+    Plane(String location, float landingTime, float takeoffTIme, int flightNumber)
+    {
+        this.location = location;
+        this.landingTime = landingTime;
+        this.takeoffTIme = takeoffTIme;
+        this.flightNumber = flightNumber;
 
+        for (Object child : Database.defects)
+        {
+            Defects newChild = (Defects) child;
+            if (newChild.flightNumber == flightNumber)
+                defects.add(newChild);
+        }
+        this.defects = defects;
+    }
 }
