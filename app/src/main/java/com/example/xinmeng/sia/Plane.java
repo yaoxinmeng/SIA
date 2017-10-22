3f4c12c5ccf4dfbe54bf937e79c9c5c2d5891634
yaoxinmeng <yaoxinmeng@gmail.com>
2017-10-22 22:15:50 +0800

    new class

---
+++ app/src/main/java/com/example/xinmeng/sia/Plane.java
@@ -0,0 +1,8 @@
+package com.example.xinmeng.sia;
+
+/**
+ * Created by Xin Meng on 22/10/2017.
+ */
+
+public class Plane {
+}
--- app/src/main/java/com/example/xinmeng/sia/TechnicianTasksMain.java
+++ app/src/main/java/com/example/xinmeng/sia/TechnicianTasksMain.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class TechnicianTasksMain extends AppCompatActivity {
 
@@ -11,6 +12,14 @@ public class TechnicianTasksMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_technician_tasks_main);
+
+        TextView location = (TextView) findViewById(R.id.Location);
+        TextView landing_Time = (TextView) findViewById(R.id.landing_time);
+        TextView takeoff_time = (TextView) findViewById(R.id.takeoff_time);
+
+        //location.setText();
+        //landing_Time.setText();
+        //takeoff_time.setText();
     }
 
     public void taskDetail(View view)
