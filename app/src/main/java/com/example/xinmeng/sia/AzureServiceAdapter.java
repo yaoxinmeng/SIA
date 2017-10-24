1d50dc65971e0d207b4249e0e91b4610980dc052
ruiggo <ruiggo@gmail.com>
2017-10-24 22:19:49 +0800

    Backbackend stuff
    Added in something to connect to the azure app service, in progress with adding other stuff.

---
+++ app/src/main/java/com/example/xinmeng/sia/AzureServiceAdapter.java
@@ -0,0 +1,46 @@
+package com.example.xinmeng.sia;
+
+import android.content.Context;
+import com.microsoft.windowsazure.mobileservices.*;
+
+import java.net.MalformedURLException;
+
+/**
+ * Created by jrnew on 24/10/2017.
+ */
+
+public class AzureServiceAdapter {
+    private String mMobileBackendUrl = "https://defectslist.azurewebsites.net/";
+    private Context mContext;
+    private MobileServiceClient mClient;
+    private static AzureServiceAdapter mInstance = null;
+
+    private AzureServiceAdapter(Context context) {
+        mContext = context;
+        try {
+            mClient = new MobileServiceClient(mMobileBackendUrl, mContext);
+        }
+        catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void Initialize(Context context) {
+        if (mInstance == null) {
+            mInstance = new AzureServiceAdapter(context);
+        } else {
+            throw new IllegalStateException("AzureServiceAdapter is already initialized");
+        }
+    }
+
+    public static AzureServiceAdapter getInstance() {
+        if (mInstance == null) {
+            throw new IllegalStateException("AzureServiceAdapter is not initialized");
+        }
+        return mInstance;
+    }
+
+    public MobileServiceClient getClient() {
+        return mClient;
+    }
+}
