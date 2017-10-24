9444b42b93a4445e9e85e19d693761bc6bffc370
ruiggo <ruiggo@gmail.com>
2017-10-25 00:08:35 +0800

    Backbackend stuff
    Updated defects fetcher. Made random changes to the unused xml code so that it stops giving error, changed gradle build file to include required dependency.

--- app/src/main/java/com/example/xinmeng/sia/MainActivity.java
+++ app/src/main/java/com/example/xinmeng/sia/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.xinmeng.sia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+
+import java.net.MalformedURLException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AzureServiceAdapter.Initialize(this);
+        try {
+            MobileServiceClient mClient = new MobileServiceClient("https://defectslist.azurewebsites.net/", this);
+        }
+        catch(MalformedURLException e){
+            e.printStackTrace();
+        }
     }
 }
