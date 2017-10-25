fecdfa30c6f2845162e47ccb374875842b81b172
yaoxinmeng <yaoxinmeng@gmail.com>
2017-10-25 11:58:05 +0800

    new classes

--- app/src/main/java/com/example/xinmeng/sia/SupervisorMain.java
+++ app/src/main/java/com/example/xinmeng/sia/SupervisorMain.java
@@ -3,6 +3,8 @@ package com.example.xinmeng.sia;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.Queue;
@@ -15,8 +17,6 @@ public class SupervisorMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supervisor_main);
-
-
         Queue planeDisplay = planesForDisplay(page);
 
         update();
@@ -46,16 +46,42 @@ public class SupervisorMain extends AppCompatActivity {
         }
     }
 
-    public void nextPage() //for next page button
+    //Button functions
+    public void nextPage(View view) //for next page button
     {
         page++;
         onResume();
     }
 
-    public void prevPage() //for previous page button
+    public void prevPage(View view) //for previous page button
     {
         if (page != 1)
             page--;
         onResume();
+    }
+
+    public void entry1(View view)
+    {
+        Plane plane = (Plane) Database.planes.get(page - 1);
+    }
+
+    public void entry2(View view)
+    {
+        Plane plane = (Plane) Database.planes.get(2 * page - 1);
+    }
+
+    public void entry3(View view)
+    {
+        Plane plane = (Plane) Database.planes.get(3 * page - 1);
+    }
+
+    public void entry4(View view)
+    {
+        Plane plane = (Plane) Database.planes.get(4 * page - 1);
+    }
+
+    public void entry5(View view)
+    {
+        Plane plane = (Plane) Database.planes.get(5 * page - 1);
     }
 }
