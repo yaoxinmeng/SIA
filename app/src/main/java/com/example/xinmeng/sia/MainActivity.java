496f9aff729e916b037acc1f51d81cb0e475a4a6
rhowardliu <rhowardliu@gmail.com>
2017-10-25 11:44:57 +0800

    MainActivity login + mobileservice

--- app/src/main/java/com/example/xinmeng/sia/MainActivity.java
+++ app/src/main/java/com/example/xinmeng/sia/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.xinmeng.sia;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
@@ -7,6 +8,7 @@ import android.widget.EditText;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
@@ -15,8 +17,11 @@ import java.net.MalformedURLException;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText username;
-    EditText password;
+    EditText username_field;
+    EditText password_field;
+    TextView username;
+    TextView password;
+    RelativeLayout layout;
 
     Button login_button;
 
@@ -25,11 +30,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AzureServiceAdapter.Initialize(this);
-        username = (EditText) findViewById(R.id.usernameField);
-        password = (EditText) findViewById(R.id.passwordField);
-
+        username_field = (EditText) findViewById(R.id.usernameField);
+        password_field = (EditText) findViewById(R.id.passwordField);
+        username = (TextView)  findViewById(R.id.username_text);
+        password = (TextView)  findViewById(R.id.password_text);
         login_button = (Button) findViewById(R.id.loginButton);
 
+        /*Intent display_intent = new Intent(this, userpwDisplay.class);
+        startService(display_intent);*/
+        enterUserPw();
         LoginButton();
 
         try {
@@ -40,9 +49,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-    public void LoginButton(){
 
-        username.setOnClickListener(
+    public void enterUserPw(){
+        username_field.setOnClickListener(
                 new View.OnClickListener(){
 
                     @Override
@@ -52,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
-        password.setOnClickListener(
+        password_field.setOnClickListener(
                 new View.OnClickListener(){
 
                     @Override
@@ -61,6 +70,24 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+       /* layout.setOnClickListener(
+                new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View view) {
+                        if (username.toString().isEmpty())
+                            username.setText("@string/type_username");
+                        if (password.toString().isEmpty())
+                            password.setText("@string/type_password");
+                    }
+                }
+        );
+        */
+
+    }
+
+    public void LoginButton(){
+
 
         login_button.setOnClickListener(
                 new View.OnClickListener(){
@@ -79,5 +106,24 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
+
+
+
     }
+    /*public class userpwDisplay extends IntentService{
+
+        public userpwDisplay() {
+            super("userpwDisplay");
+        }
+
+        @Override
+        protected void onHandleIntent(@Nullable Intent intent) {
+            if (username_field.getText().toString().isEmpty())
+                username.setText("@string/type_username");
+            else
+                username.setText("");
+
+        }
+    }*/
+
 }
