e1faca2a4cd1c23231c087427e644ffae7a30322
yaoxinmeng <yaoxinmeng@gmail.com>
2017-10-27 03:07:20 +0800

    we are screwed

--- app/src/main/java/com/example/xinmeng/sia/TechnicianEquipment.java
+++ app/src/main/java/com/example/xinmeng/sia/TechnicianEquipment.java
@@ -1,16 +1,28 @@
 package com.example.xinmeng.sia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.System.currentTimeMillis;
+
 public class TechnicianEquipment extends AppCompatActivity {
     private Technicians technician;
     private String techID;
-    private Plane currentPlane;
+    private Plane firstPlane;
     private ArrayList<String> equipment = new ArrayList<>();
+
+    TextView arrTime;
+    TextView depTime;
+    TextView bay;
+    TextView type;
+    TextView status;
+    TextView regn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +40,58 @@ public class TechnicianEquipment extends AppCompatActivity {
                 break;
             }
         }
-        currentPlane = technician.planes.element();
-        for (Object child : currentPlane.defects)
+        firstPlane = technician.currentPlane;
+        for (Object child : firstPlane.defects)
         {
             String parts = ((Defects) child).parts;
             equipment.add(parts);
         }
+
+        regn = (TextView) findViewById(R.id.flight_regn);
+        bay = (TextView) findViewById(R.id.flight_bay);
+        arrTime = (TextView) findViewById(R.id.flight_time_arrival);
+        depTime = (TextView) findViewById(R.id.flight_time_departure);
+        type = (TextView) findViewById(R.id.flight_actype);
+        status = (TextView) findViewById(R.id.flight_status);
+
+        setTexts();
+    }
+
+    private void setTexts()
+    {
+        regn.setText(firstPlane.regn);
+        bay.setText(firstPlane.bay);
+        type.setText(firstPlane.type);
+
+        float arrtime = (float) (firstPlane.arrTime.getTime());
+        int hour = (int) arrtime;
+        int minute = (int) (100 * (arrtime - ((float) hour)));
+        if (hour < 10)
+        {
+            if (minute < 10)
+                arrTime.setText("0" + String.valueOf(hour) + ":0" + String.valueOf(minute));
+            else
+                arrTime.setText("0" + String.valueOf(hour) + ":" + String.valueOf(minute));
+        }
+        else
+        {
+            if (minute < 10)
+                arrTime.setText(String.valueOf(hour) + ":0" + String.valueOf(minute));
+            else
+                arrTime.setText(String.valueOf(hour) + ":" + String.valueOf(minute));
+        }
+
+        if (currentTimeMillis() < firstPlane.arrTime.getTime())
+            status.setText("Arrived");
+        else if (firstPlane.delay)
+            status.setText("Delayed");
+        else
+            status.setText("On Time");
+    }
+
+    public void backButton(View view)
+    {
+        Intent intent = new Intent(TechnicianEquipment.this, TechnicianMain.class);
+        startActivity(intent);
     }
 }
