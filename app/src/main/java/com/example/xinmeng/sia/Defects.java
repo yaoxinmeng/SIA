7436bfb7cfd70da0c8aa8d75bc3f5768f8f60495
yaoxinmeng <yaoxinmeng@gmail.com>
2017-10-26 16:41:45 +0800

    we are screwed

--- app/src/main/java/com/example/xinmeng/sia/Defects.java
+++ app/src/main/java/com/example/xinmeng/sia/Defects.java
@@ -1,5 +1,7 @@
 package com.example.xinmeng.sia;
 
+import java.util.Date;
+
 /**
  * Created by Xin Meng on 22/10/2017.
  */
@@ -8,6 +10,10 @@ public class Defects {
     public String category;
     public String description;
     public String regn;
+    public String dateRaised;
+    public String fleet;
+    public String stn;
+    public String number;
     public int classCode;
 
     public boolean inProgress;
@@ -17,15 +23,33 @@ public class Defects {
     public String parts;
     public String action;
     public int priority;
-
+    public String deferralReason;
     public String techID; //techID assigned to this defect
 
-    public Defects(String category, String description, String regn, int classCode) {
+    public Defects(String category, String description, String regn, String dateRaised, String fleet, String stn, String number, String classCode) {
         this.category = category;
         this.description = description;
         this.regn = regn;
-        this.classCode = classCode;
+        this.dateRaised = dateRaised;
+        this.fleet = fleet;
+        this.stn = stn;
+        this.number = number;
+
+        if (classCode.equals("Economy"))
+            this.classCode = Database.Economy;
+        else if (classCode.equals("Premium"))
+            this.classCode = Database.Premium;
+        else if (classCode.equals("Business"))
+            this.classCode = Database.Business;
+        else if (classCode.equals("First"))
+            this.classCode = Database.First;
+
         this.inProgress = false;
-        this.resolved = true;
+        this.resolved = false;
+        this.parts = null;
+        this.action = null;
+        this.priority = 0;
+        this.deferralReason = null;
+        this.techID = null;
     }
 }
