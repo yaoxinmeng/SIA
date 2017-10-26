bf934bf8641428bdafbd80a63209d05ad07244a4
yaoxinmeng <yaoxinmeng@gmail.com>
2017-10-26 14:34:24 +0800

    new classes

--- app/src/main/java/com/example/xinmeng/sia/TechnicianEquipment.java
+++ app/src/main/java/com/example/xinmeng/sia/TechnicianEquipment.java
@@ -3,8 +3,13 @@ package com.example.xinmeng.sia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TechnicianEquipment extends AppCompatActivity {
     private Technicians technician;
+    private Plane currentPlane;
+    private List equipment = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,12 @@ public class TechnicianEquipment extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         technician = (Technicians) extras.getSerializable("TECH");
+        currentPlane = (Plane) technician.planes.element();
+        for (Object child : currentPlane.defects)
+        {
+            String parts = ((Defects) child).parts;
+            equipment.add(parts);
+        }
+
     }
 }
