97644c6ca14b1b89de19054c92f81a21405f9f4f
yaoxinmeng <yaoxinmeng@gmail.com>
2017-10-26 22:01:59 +0800

    we are screwed

--- app/src/main/java/com/example/xinmeng/sia/TechnicianTasksMain.java
+++ app/src/main/java/com/example/xinmeng/sia/TechnicianTasksMain.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.Serializable;
+
+import static java.lang.System.currentTimeMillis;
+
 public class TechnicianTasksMain extends AppCompatActivity {
     private Technicians technician;
     TextView arrTime;
@@ -29,11 +33,58 @@ public class TechnicianTasksMain extends AppCompatActivity {
         depTime = (TextView) findViewById(R.id.flight_time_departure);
         type = (TextView) findViewById(R.id.flight_actype);
         status = (TextView) findViewById(R.id.flight_status);
+
+        setTexts();
+    }
+
+    private void setTexts()
+    {
+        Plane firstPlane = technician.currentPlane;
+        regn.setText(firstPlane.regn);
+        bay.setText(firstPlane.bay);
+        type.setText(firstPlane.type);
+
+        float arrtime = (float) (firstPlane.arrTime.getTime());
+        int hour = (int) arrtime;
+        int minute = (int) (100 * (arrtime - ((float) hour)));
+        if (hour < 10)
+        {
+            if (minute < 10)
+                arrTime.setText("0" + String.valueOf(hour) + ":0" + String.valueOf(minute));
+            else
+                arrTime.setText("0" + String.valueOf(hour) + ":" + String.valueOf(minute));
+        }
+        else
+        {
+            if (minute < 10)
+                arrTime.setText(String.valueOf(hour) + ":0" + String.valueOf(minute));
+            else
+                arrTime.setText(String.valueOf(hour) + ":" + String.valueOf(minute));
+        }
+
+        if (currentTimeMillis() < technician.currentPlane.arrTime.getTime())
+            status.setText("Arrived");
+        else if (firstPlane.delay)
+            status.setText("Delayed");
+        else
+            status.setText("On Time");
     }
 
     public void taskDetail(View view)
     {
         Intent intent = new Intent(TechnicianTasksMain.this, TechnicianTaskDetail.class);
+        startActivity(intent);
+    }
+
+    public void refresh(View view)
+    {
+        setTexts();
+    }
+
+    public void equipment (View view)
+    {
+        Intent intent = new Intent(TechnicianTasksMain.this, TechnicianEquipment.class);
+        intent.putExtra("TECH", (Serializable) technician);
         startActivity(intent);
     }
 }
