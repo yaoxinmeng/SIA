98a67732e2aec3599121738e48fdbb9912e0c48c
rhowardliu <rhowardliu@gmail.com>
2017-10-27 03:38:52 +0800

    MainActivity login + mobileservice

---
+++ app/src/main/java/com/example/xinmeng/sia/SkyOrangeBgFragment.java
@@ -0,0 +1,18 @@
+package com.example.xinmeng.sia;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SkyOrangeBgFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.skyorangebg,container,false);
+        return view;
+    }
+}
