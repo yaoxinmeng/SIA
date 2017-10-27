d14acad8be5680e9a5157bfb3c1f0b2eb59530c4
ruiggo <ruiggo@gmail.com>
2017-10-27 15:58:36 +0800

    ermm
    ermmm

--- app/src/main/java/com/example/xinmeng/sia/Database.java
+++ app/src/main/java/com/example/xinmeng/sia/Database.java
@@ -61,7 +61,7 @@ public class Database {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
